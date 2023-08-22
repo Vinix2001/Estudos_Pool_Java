4f2b203234d4e956940c7f5760ad5fc9b554e6f8
Vinicius Araújo Mendes <110427550+Vinix2001@users.noreply.github.com>
2023-08-22 11:43:40 -0300

    Add files via upload

---
+++ Encap/ControleRmt.java
@@ -0,0 +1,11 @@
+package Encap;
+
+public class ControleRmt{
+    public static void main(String[] args) {
+        Controle c = new Controle();
+        c.ligar();
+        c.abrirMenu();
+            
+        }
+    
+}
